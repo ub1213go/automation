e84c252f0ac2326104dd9d2aa8698ce8f77386ac
ub1213go <ub1213go@gmail.com>
2023-12-31 14:20:57 +0800

    update

--- AConsole/Program.cs
+++ AConsole/Program.cs
@@ -166,5 +166,64 @@ namespace AConsole
             }
         }
     }
+
+    public interface IObservable
+    {
+        void Subscription(IObserver observer, ConsoleKey key);
+        void Notify(ConsoleKey key);
+    }
+    public interface IObserver
+    {
+        void Update();
+    }
+    public class Menu : IObservable
+    {
+        private int loopLimit = 100;
+        private HashSet<IObserver> Subscribers
+            = new HashSet<IObserver>();
+        private Dictionary<ConsoleKey, IObserver> SpecifyNotify
+            = new Dictionary<ConsoleKey, IObserver>();
+        public void Subscription(IObserver observer, ConsoleKey key)
+        {
+            Subscribers.Add(observer);
+
+            if(!SpecifyNotify.ContainsKey(key))
+            {
+                SpecifyNotify.Add(key, observer);
+            }
+            else
+            {
+                SpecifyNotify[key] = observer;
+            }
+        }
+        public void Notify(ConsoleKey key)
+        {
+            if (SpecifyNotify.TryGetValue(key, out var val))
+            {
+                val.Update();
+            }
+        }
+        public IEnumerable<int> Run()
+        {
+            for (int i = 0; i < loopLimit; i++)
+            {
+                Notify(Console.ReadKey(true).Key);
+                yield return i;
+            }
+        }
+    }
+    public class KeyEvent : IObserver
+    {
+        private Action action { get; set; }
+        public KeyEvent(Action action)
+        {
+            this.action = action;
+        }
+        public void Update()
+        {
+            action();
+        }
+    }
+
 }
 
