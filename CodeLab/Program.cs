8d8ebf9284e7443c2a267f73f711a8d4e7085da0
ub1213go <ub1213go@gmail.com>
2023-12-31 20:39:31 +0800

    update

--- CodeLab/Program.cs
+++ CodeLab/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ASqlite;
 using Dapper;
+using System.Text;
 
 namespace Program
 {
@@ -9,14 +10,14 @@ namespace Program
         {
             var sql = new Sqlite();
 
-            //sql.DropTable(typeof(AutomationFlow));
-            //sql.CreateTable(typeof(AutomationFlow));
-            //sql.Create(new AutomationFlow()
-            //{
-            //    FlowName = "自動報稅",
-            //    FlowSeries = 1,
-            //    FlowContent = "",
-            //});
+            sql.DropTable(typeof(AutomationFlow));
+            sql.CreateTable(typeof(AutomationFlow));
+            sql.Create(new AutomationFlow()
+            {
+                FlowName = "自動報稅",
+                FlowSeries = 1,
+                FlowContent = "",
+            });
 
             var items = sql.Read<AutomationFlow>(0, 10);
             foreach (var item in items)
@@ -42,20 +43,27 @@ namespace Program
         public long FlowSeries { get; set; }
         [SC(Name = "FlowContent", Type = SCAttribute.CType.NVARCHAR, IsIndex = true, Len = 4000)]
         public string? FlowContent { get; set; }
-        //[SC(Name = "CreateDateTime", Type = SCAttribute.CType.DATETIME, Default = "CURRENT_TIMESTAMP")]
-        [SC(Name = "CreateDateTime", Type = SCAttribute.CType.DATETIME, Default = "(DATETIME('now','localtime')")]
-        public DateTime CreateDateTime { get; set; }
-        [SC(Name = "CreateUser", Type = SCAttribute.CType.VARCHAR, Len = 20, Default = "''")]
+        [SC(Name = "CreateDateTime", Type = SCAttribute.CType.DATETIME, Default = "(DATETIME('now', 'localtime'))")]
+        public DateTime? CreateDateTime { get; set; }
+        [SC(Name = "CreateUser", Type = SCAttribute.CType.VARCHAR, Len = 20, Default = "'sys'")]
         public string? CreateUser { get; set; }
-        //[SC(Name = "UpdateDateTime", Type = SCAttribute.CType.DATETIME, Default = "CURRENT_TIMESTAMP")]
-        [SC(Name = "UpdateDateTime", Type = SCAttribute.CType.DATETIME, Default = "(DATETIME('now','localtime')")]
-        public DateTime UpdateDateTime { get; set; }
-        [SC(Name = "UpdateUser", Type = SCAttribute.CType.VARCHAR, Len = 20, Default = "''")]
+        [SC(Name = "UpdateDateTime", Type = SCAttribute.CType.DATETIME, Default = "(DATETIME('now', 'localtime'))")]
+        public DateTime? UpdateDateTime { get; set; }
+        [SC(Name = "UpdateUser", Type = SCAttribute.CType.VARCHAR, Len = 20, Default = "'sys'")]
         public string? UpdateUser { get; set; }
 
         public override string ToString()
         {
-            return $"{ID}. FlowName: {FlowName}, FlowContent: {FlowContent} CreateDateTime: {CreateDateTime}, CreateUser: {CreateUser}";
+            var sb = new StringBuilder();
+            sb.AppendLine($"{nameof(ID)}: {ID}");
+            sb.AppendLine($"{nameof(FlowName)}: {FlowName}");
+            sb.AppendLine($"{nameof(FlowSeries)}: {FlowSeries}");
+            sb.AppendLine($"{nameof(FlowContent)}: {FlowContent}");
+            sb.AppendLine($"{nameof(CreateDateTime)}: {CreateDateTime}");
+            sb.AppendLine($"{nameof(CreateUser)}: {CreateUser}");
+            sb.AppendLine($"{nameof(UpdateDateTime)}: {UpdateDateTime}");
+            sb.AppendLine($"{nameof(UpdateUser)}: {UpdateUser}");
+            return sb.ToString();
         } 
     }
 }
